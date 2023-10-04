03fab20d995e5766c37916a63f38988312caf2ca
Yannawut tin <58814744+somearthzaza@users.noreply.github.com>
2023-10-05 01:09:48 +0700

    add unit_test

---
+++ unit_test.py
@@ -0,0 +1,13 @@
+#unit test
+import unittest
+import app 
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_test(self):
+        self.assertEqual(app.hello_world(), "test")
+
+
+if __name__ == '__main__':
+    unittest.main()
